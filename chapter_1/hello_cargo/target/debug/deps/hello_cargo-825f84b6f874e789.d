70bfb82e975728c46ddb1a3ded9fa22ffb7d83ca
Calvin Lapp <lapp.cd@gmail.com>
2022-03-31 19:30:08 -0400

    Finished chapter 1

---
+++ chapter_1/hello_cargo/target/debug/deps/hello_cargo-825f84b6f874e789.d
@@ -0,0 +1,5 @@
+/home/runescape/projects/rustbook/chapter_1/hello_cargo/target/debug/deps/hello_cargo-825f84b6f874e789: src/main.rs
+
+/home/runescape/projects/rustbook/chapter_1/hello_cargo/target/debug/deps/hello_cargo-825f84b6f874e789.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/home/runescape/projects/rustbook/chapter_1/hello_cargo/target/debug/hello_cargo: /home/runescape/projects/rustbook/chapter_1/hello_cargo/src/main.rs
