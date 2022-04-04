ffef349e31fac48759dc53869fa3076eb7353b48
Calvin Lapp <lapp.cd@gmail.com>
2022-04-04 19:44:19 -0400

    Chapter 3 done

---
+++ chapter_3/invalid_array_access/target/debug/deps/invalid_array_access-a13ac3263d6d1269.d
@@ -0,0 +1,5 @@
+/home/runescape/projects/rustbook/chapter_3/invalid_array_access/target/debug/deps/invalid_array_access-a13ac3263d6d1269: src/main.rs
+
+/home/runescape/projects/rustbook/chapter_3/invalid_array_access/target/debug/deps/invalid_array_access-a13ac3263d6d1269.d: src/main.rs
+
+src/main.rs:
---
+++ chapter_3/variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/runescape/projects/rustbook/chapter_1/variables/target/debug/variables: /home/runescape/projects/rustbook/chapter_1/variables/src/main.rs
